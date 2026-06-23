b0b9382d30486aa552cddf4531aa055aa2cb9020
ZHAOPINGAN <pingan.zhao@bl.com>
2026-06-23 14:49:46 +0800

    feat: add TooltipProvider for sidebar tooltip support

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { TooltipProvider } from '@/components/ui/tooltip'
 import './index.css'
 import App from './App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <TooltipProvider>
+      <App />
+    </TooltipProvider>
   </StrictMode>,
 )
