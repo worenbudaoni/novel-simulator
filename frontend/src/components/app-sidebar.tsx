4fd7116496f0b16b98be50ceaed19b4b9a3fa5f9
ZHAOPINGAN <pingan.zhao@bl.com>
2026-06-25 10:17:00 +0800

    fix: also disable active/pressed highlight on sidebar logo

--- frontend/src/components/app-sidebar.tsx
+++ frontend/src/components/app-sidebar.tsx
@@ -44,7 +44,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
         <SidebarMenu>
           <SidebarMenuItem>
             <SidebarMenuButton
-              className="data-[slot=sidebar-menu-button]:p-1.5! hover:bg-transparent hover:text-inherit cursor-default"
+              className="data-[slot=sidebar-menu-button]:p-1.5! hover:bg-transparent hover:text-inherit active:bg-transparent data-active:bg-transparent cursor-default"
               render={<Link to="/player" />}
             >
               <CommandIcon className="size-5!" />
