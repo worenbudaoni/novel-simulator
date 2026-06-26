f2584857c4b5cc8aad00904a03f532420817b6af
ZHAOPINGAN <pingan.zhao@bl.com>
2026-06-26 10:37:24 +0800

    chore: remove unused Collectors import from NodeService

--- src/main/java/com/novel/simulator/service/NodeService.java
+++ src/main/java/com/novel/simulator/service/NodeService.java
@@ -11,7 +11,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class NodeService {
